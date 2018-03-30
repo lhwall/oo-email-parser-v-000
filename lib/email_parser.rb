e2cdcd3ea561911b05005af7cc4505d52fbf36fd
Lucy Wall <lhwall@gmail.com>
2018-03-30 17:18:46 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
   
   def parse
-    parsed_emails = @emails.split(/[\,\s]*/).uniq{|email| email}
+    parsed_emails = @emails.split(/[\, ]*/).uniq{|email| email}
     parsed_emails
   end 
   
